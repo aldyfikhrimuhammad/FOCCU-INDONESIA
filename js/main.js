1594a4d848e5594d49e393da4be785c9fc7859ce
aldyfikhrimuhammad <aldyfikhrimuhammad@gmail.com>
2022-01-03 09:06:56 +0700

    Fixing dropdown menu & content

--- js/main.js
+++ js/main.js
@@ -12,19 +12,6 @@
     }
   });
 
-  // Back to top button
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 100) {
-      $(".back-to-top").fadeIn("slow");
-    } else {
-      $(".back-to-top").fadeOut("slow");
-    }
-  });
-  $(".back-to-top").click(function () {
-    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
-    return false;
-  });
-
   // Initiate the wowjs animation library
   new WOW().init();
 
@@ -127,5 +114,4 @@
     loop: true,
     items: 1,
   });
-
 })(jQuery);
